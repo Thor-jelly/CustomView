3993808341810117e5d321f64c032e73b352f8ac
dongdong.wu <withwudongdong@gmail.com>
2019-07-04 16:43:41 +0800

    自定义 字体加粗

--- app/src/main/java/com/jelly/thor/customview/MainActivity.kt
+++ app/src/main/java/com/jelly/thor/customview/MainActivity.kt
@@ -18,8 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         //设置新字体
         clickFontStyle.setFontStyle(FontStyleEnum.DIN_ALTERNATE_BOLD)
+        clickFontStyleBold.setFontStyle(FontStyleEnum.DIN_ALTERNATE_BOLD)
         //设置新字体加粗
-        //clickFontStyle.paint.isFakeBoldText = true
+        clickFontStyleBold.paint.isFakeBoldText = true
 
         clickSnapHelpTest.setOnClickListener {
             startActivity(Intent(this, SnapHelpActivity::class.java))
